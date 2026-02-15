3257e1a5c60753ac09f30d5f1989d6a94155cf2c
amir-nemati <166291146+amir-nemati@users.noreply.github.com>
2026-02-15 09:50:28 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-fetch("songs.json")
-  .then(res => res.json())
-  .then(data => {
-    const container = document.getElementById("musicList");
-    const player = document.getElementById("player");
+const musicList = document.getElementById("musicList");
+const player = document.getElementById("player");
 
-    data.forEach(song => {
-      const div = document.createElement("div");
-      div.className = "card";
-      div.innerHTML = `
-        <h3>${song.title}</h3>
-        <p>${song.artist}</p>
-      `;
-      div.onclick = () => {
-        player.src = song.url;
-        player.play();
-      };
-      container.appendChild(div);
-    });
+let songs = JSON.parse(localStorage.getItem("songs")) || [];
+
+function renderSongs() {
+  musicList.innerHTML = "";
+  songs.forEach((song, index) => {
+    const div = document.createElement("div");
+    div.className = "card";
+    div.innerHTML = `
+      <h3>${song.title}</h3>
+      <p>${song.artist}</p>
+    `;
+    div.onclick = () => {
+      player.src = song.url;
+      player.play();
+    };
+    musicList.appendChild(div);
   });
+}
+
+renderSongs();
